a48dcc32ff341b0542de6bdaaad92b25aa463da4
Seanghor Hai <haiseanghor20@kit.edu.kh>
2022-02-15 22:52:14 +0700

    edit profile

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(cors({ origin: 'http://localhost:4200' }));
 
-letport = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
