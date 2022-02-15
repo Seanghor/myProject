6a9632f6744204ae7ec460140ce36c393694104a
ChanDara Leav <chandara@nokor-tech.com>
2022-02-16 01:54:24 +0700

    save

--- db.js
+++ db.js
@@ -1,25 +1,13 @@
-// // const mongoose = require('mongoose');
 
-
-// // const dbConn = mongoose.connect('mongodb://localhost:27017/CountryManager',(err)=>{
-// //     if(!err){
-// //         console.log("MongoDB connection Succed")
-// //     }else{
-// //         console.log("Error in DB connection"+JSON.stringify(err))
-// //     }
-// // })
-
-// module.exports = dbConn;
 
 const mongoose = require('mongoose');
 
 //update:
-// const dbURI = "mongodb+srv://emp_detail:seanghor12345@cluster0.d3x24.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const dbURI = "mongodb://emp_detail:seanghor12345@cluster0-shard-00-00.d3x24.mongodb.net:27017,cluster0-shard-00-01.d3x24.mongodb.net:27017,cluster0-shard-00-02.d3x24.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-cbbbyv-shard-0&authSource=admin&retryWrites=true&w=majority"
 const dbConn = async() => {
     try {
         // mongodb connection string
-        const con = await mongoose.connect(dbURI, {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
@@ -30,3 +18,4 @@ const dbConn = async() => {
     }
 };
 
+module.exports = dbConn;
